7433490b3ff4f8c1b4f14c8daecfbe844bd009f4
GGo3 <voloshynmv@gmail.com>
2020-10-09 20:07:31 +0300

    added checkArr function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,44 @@
 const bordSize = 5;
 let boardColumns = [];
 
+//****************1) создание двумерного массива ****************
 
-const createArr = (newArr, value) => {
-  for (let i = 0; i < bordSize; i++) {
-    let boardRow = [];
-    for (let k = 0; k < bordSize; k++){
-      boardRow[k] = value;
-    }
-    newArr[i] = boardRow;
+for (let i = 0; i < bordSize; i++) {
+  let boardRows = [];
+  for (let k = 0; k < bordSize; k++){
+    boardRows[k] = k;
   }
+  boardColumns[i] = boardRows;
 }
 
-createArr(boardColumns, null);
+//**************** */ 2) функция очистки массива ****************
+
+const clearArr = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < arr[i].length; k++) {
+      arr[i][k] = null;
+    }
+  }
+};
+clearArr(boardColumns);
+
+//**************** 3) функция подсчета ненулевых(непустых) клеток во всём массиве ****************
+
+const checkArr = (arrCheck) => {
+  let clearValue = 0;
+  for (let i = 0; i < arrCheck.length; i++) {
+    for (let k = 0; k < arrCheck[i].length; k++) {
+      if (arrCheck[i][k] == null){
+        clearValue++;
+      }
+    }
+  }
+  console.log(clearValue);
+};
+
+checkArr(boardColumns);
+
+
 
 console.table(boardColumns);
+
