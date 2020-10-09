4bcd0d25c1353cc17d8a5728fa4485489c80383d
GGo3 <voloshynmv@gmail.com>
2020-10-09 19:37:58 +0300

    added function clearArr

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@ const bordSize = 5;
 let boardColumns = [];
 
 
-for (let i = 0; i < bordSize; i++) {
-  let boardRow = [];
-  for (let k = 0; k < bordSize; k++){
-    boardRow[k] = k;
+const createArr = (newArr, value) => {
+  for (let i = 0; i < bordSize; i++) {
+    let boardRow = [];
+    for (let k = 0; k < bordSize; k++){
+      boardRow[k] = value;
+    }
+    newArr[i] = boardRow;
   }
-  boardColumns[i] = boardRow;
 }
-console.log(boardColumns);
+
+createArr(boardColumns, null);
+
+console.table(boardColumns);
